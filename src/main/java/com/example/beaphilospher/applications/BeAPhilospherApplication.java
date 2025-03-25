b23a17f04428fb80d58bd65b618995dbbbcbdf00
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2025-03-25 11:00:23 +0100

    Test Push😭

---
+++ src/main/java/com/example/beaphilospher/applications/BeAPhilospherApplication.java
@@ -0,0 +1,13 @@
+package com.example.beaphilospher.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAPhilospherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAPhilospherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beaphilospher/BeAPhilospherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beaphilospher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAPhilospherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
